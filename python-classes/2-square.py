bfe7a456173e7b343f92163fac6adcd0903cf4a3
Your Name <you@example.com>
2025-12-23 07:16:09 +0000

    Add Square area method

---
+++ python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a Square class with size validation and area calculation."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
